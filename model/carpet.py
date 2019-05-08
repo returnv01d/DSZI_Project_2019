5fa3d4dbd26ec3fa48643d4ba1ea14138b6d1c2f
zarzyckamar <zarzyckamar@gmail.com>
2019-05-08 15:32:53 +0200

    add carpet sprite and model

---
+++ model/carpet.py
@@ -0,0 +1,12 @@
+from sprites.carpetSprite import CarpetSprite
+
+class Carpet:
+
+    def __init__(self):
+        self.sprite = None
+
+    def create_sprite(self, width, height):
+        self.sprite = CarpetSprite(width, height)
+
+    def __repr__(self):
+        return "Carpet"
