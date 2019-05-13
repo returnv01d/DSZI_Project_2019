a337155de25bfbd9aa5f77d7ee8feacf3d053e5e
returnv01d <bartlomiej.winiarski88@gmail.com>
2019-05-13 16:32:42 +0200

    Change 'image_path' variable to 'image_name' which only specifies name of image.

--- sprites/carpetSprite.py
+++ sprites/carpetSprite.py
@@ -3,8 +3,8 @@ from sprites.basic_sprite import BasicSprite
 
 class CarpetSprite(BasicSprite):
     def __init__(self, width, height):
-        image_path = "images/carpet.png"
-        BasicSprite.__init__(self, width, height, image_path)
+        self.image_name = "carpet.png"
+        BasicSprite.__init__(self, width, height, self.image_name)
 
 
 
