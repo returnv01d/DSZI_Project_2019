54813a418fb90b118fe14867cc3362b43704fb7d
returnv01d <bartlomiej.winiarski88@gmail.com>
2019-05-18 08:31:54 +0200

    Add methods 'give_order' and 'check if interaction possible'

--- model/kitchen.py
+++ model/kitchen.py
@@ -3,9 +3,10 @@ from model.order import Order
 from model.move import Move
 
 class Kitchen:
-    def __init__(self):
+    def __init__(self, orders):
         self.sprite = None
-        self.orders = []
+        self.orders = orders
+        self.given_orders = []
 
     def create_sprite(self, width, height):
         self.sprite = KitchenSprite(width, height)
@@ -17,14 +18,20 @@ class Kitchen:
     def __str__(self):
         return 'K'
 
+    def give_order(self, order):
+        order.is_taken_from_kitchen = True
+        self.given_orders.append(order)
+        self.orders.remove(order)
+        self.sprite.update_status(self.statuses())
+
     def statuses(self):
-        orders_as_strings = [repr(order) for order in self.orders]
+        orders_as_strings = [repr(order) for order in self.orders if order.is_taken_from_kitchen == False]
         return orders_as_strings
 
     def waiting_orders(self):
         return [order for order in self.orders if order.is_taken_from_kitchen is False]
 
-    def check_if_next_move_possible(self, previous_move):
-        if previous_move.type == Move.TAKE_ORDER:
-            return False
-        return True
+    def check_if_interaction_with_waiter_possible(self, waiter):
+        if len(waiter.heldOrders) < 2 and self.orders:
+            return True
+        return False
