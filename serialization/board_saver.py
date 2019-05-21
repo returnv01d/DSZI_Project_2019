11624d889b0de5153d8e156aecafd0a44f026424
Anna Czaplicka <anncza2@st.amu.edu.pl>
2019-05-21 20:42:58 +0200

    added writing orders to new_board.txt

--- serialization/board_saver.py
+++ serialization/board_saver.py
@@ -8,6 +8,13 @@ class BoardSaver:
         file.write("\n")
         file.write("{0} {1}".format(str(board.waiter.y), str(board.waiter.x)))
         file.write("\n")
+        file.write(str(len(board.kitchen.waiting_orders)))
+        file.write("\n")
+
+
+        for j in range (0, len(board.kitchen.waiting_orders)):
+            file.write("{0} {1}".format(str(board.kitchen.waiting_orders[j].table_id), str(board.kitchen.waiting_orders[j].order_name)))
+            file.write("\n")
 
         for i in range(0, board.board_size):
             for j in range(0, board.board_size):
