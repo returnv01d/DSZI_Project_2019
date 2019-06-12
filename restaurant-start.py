da697ac7c9ccc6a9415368b58bb4904fae79498d
martyna <zarzyckamar@gmail.com>
2019-06-12 11:47:14 +0200

    official main

--- restaurant-start.py
+++ restaurant-start.py
@@ -27,66 +27,64 @@ fpsClock = pygame.time.Clock()
 DISPLAYSURF = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), 0, 32)
 background_image = pygame.image.load('images/background_image.png')
 
+
+
+files = []
+files.append('boards/board5.txt')
+files.append('boards/board3.txt')
+files.append('boards/board6.txt')
+
 # IMPORTANT! READ BEFORE ADDING YOUR ALGORITHM. ADD YOUR ALGORITHM CLASS, NOT FUNCTION.
 # YOUR CLASS SHOULD HAVE "NAME" FIELD AND "SOLUTION" FIELD WHERE YOU MUST PUT YOUR LIST WITH SOLUTION MOVES.
 # ADD YOU ALGORITHM CLASS HERE.
-algorithms = [BestFirstSearch, DFS]
+algorithms = [DFS, BFS, BestFirstSearch]
 
 print("hello in شروانشاه restaurant!!")
 
 while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == KEYDOWN:
-            if event.key == K_UP:
-                board.do(Move(MoveType.UP))
-            if event.key == K_DOWN:
-                board.do(Move(MoveType.DOWN))
-            if event.key == K_RIGHT:
-                board.do(Move(MoveType.RIGHT))
-            if event.key == K_LEFT:
-                board.do(Move(MoveType.LEFT))
-            if event.key == K_o:
-                board.do(Move(MoveType.TAKE_ORDER, first_order=board.kitchen.orders[-1]))
-            if event.key == K_p:
-                print(DFS.dfs(board, [], Move(MoveType.EMPTY_MOVE)))
+    for file in files:
 
-    for algo in algorithms:
-        board = None
-        Order.id = 0
-        Table.id = 0
-        board = copy.deepcopy(BoardLoader.load_board_from_file('boards/new_board.txt'))
-        print(algo)
-        if algo == DFS:
-            print("dfsik")
-            DFS.dfs(board, [], Move(MoveType.EMPTY_MOVE))
+        for algo in algorithms:
+            board = None
+            solution = None
+            Order.id = 0
+            Table.id = 0
+            board = copy.deepcopy(BoardLoader.load_board_from_file(file))
+            print(algo)
+            if algo == DFS:
+                print("dfsik")
+                DFS.dfs(board, [], Move(MoveType.EMPTY_MOVE))
+                solution = DFS.soulution
+                DFS.found_solution = None
+                DFS.soulution = None
+            elif algo == BFS:
+                BFS.bfs(board, [], Move(MoveType.EMPTY_MOVE))
+                solution = BFS.soulution
+                BFS.soulution = None
+                BFS.found_solution = None
+            elif algo == BestFirstSearch:
+                BestFirstSearch.best_first(board, [], [], Move(MoveType.EMPTY_MOVE))
+                solution = BestFirstSearch.soulution
+                BestFirstSearch.soulution = None
+                BestFirstSearch.found_solution = None
 
-        elif algo == BFS:
+            pygame.display.set_caption("Restaurant - doing {0}".format(algo.name))
+            solution = list(reversed(solution))
+            print("otrzymana solucja: ")
+            print(list(reversed(solution)))
+            Order.id = 0
+            Table.id = 0
+            animation_board = BoardLoader.load_board_from_file(file)
+            sprites = animation_board.to_sprite_group(WINDOW_WIDTH, WINDOW_HEIGHT)
+            pygame.display.set_caption("Restaurant - finished {0}. Doing solution...".format(algo.name))
+            for i in range(len(solution)):
+                move = solution.pop()
+                pygame.display.set_caption("Restaurant - finished {0}. Doing solution....Current move: {1}".format(algo.name, move))
+                animation_board.do(move)
+                time.sleep(STEP_TIME)
 
-        elif algo == BestFirstSearch:
-            BestFirstSearch.best_first(board, [], [], Move(MoveType.EMPTY_MOVE))
-            solution = BestFirstSearch.soulution
-
-        pygame.display.set_caption("Restaurant - doing {0}".format(algo.name))
-        solution = list(reversed(solution))
-        print("otrzymana solucja: ")
-        print(list(reversed(solution)))
-        Order.id = 0
-        Table.id = 0
-        animation_board = BoardLoader.load_board_from_file('boards/new_board.txt')
-        sprites = animation_board.to_sprite_group(WINDOW_WIDTH, WINDOW_HEIGHT)
-        pygame.display.set_caption("Restaurant - finished {0}. Doing solution...".format(algo.name))
-        for i in range(len(solution)):
-            move = solution.pop()
-            pygame.display.set_caption("Restaurant - finished {0}. Doing solution....Current move: {1}".format(algo.name, move))
-            animation_board.do(move)
-            time.sleep(STEP_TIME)
-
-            DISPLAYSURF.blit(background_image, (0, 0))
-            sprites.draw(DISPLAYSURF)
-            pygame.display.flip()
-            fpsClock.tick(FPS)
-        time.sleep(1.0)
-
+                DISPLAYSURF.blit(background_image, (0, 0))
+                sprites.draw(DISPLAYSURF)
+                pygame.display.flip()
+                fpsClock.tick(FPS)
+            time.sleep(1.0)
